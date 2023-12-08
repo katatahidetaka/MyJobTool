1a5e3922cb9d066765dc99e96042268bdbb5b267
katatahidetaka <katata@ibsnet.co.jp>
2023-12-08 15:14:19 +0900

    キーワード検索処理をServiceクラスに切り分け

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Tag;
 use App\Models\Category;
 use Illuminate\Contracts\View\View;
-use App\Models\Post;
 use App\Http\Requests\searchWordRequest;
+use App\Services\SearchService;
 
 class SearchController extends Controller
 {
-    public function searchByTags(int $id, Category $category) :View
+    public function searchByTags(int $id, Category $category) :mixed
     {
         $posts = [];
         $tagName = "";
@@ -26,27 +26,12 @@ class SearchController extends Controller
         return view('search.tag',compact('posts', 'tagName', 'categoryList'));
     }
     
-    public function searchByKeywords(searchWordRequest $request, Category $category) :View
+    public function searchByKeywords(searchWordRequest $request, Category $category, SearchService $service) :View
     {
         $inputKeyword = $request->inputKeyword;
-        $keywords = [];
-        $keywords = self::procKeywords($inputKeyword);
-        foreach ($keywords as $keyword) {
-            $keyword = addcslashes($keyword, '%_\\');
-            $posts = Post::where('title', 'like', '%'. $keyword. '%')
-            ->orwhere('content', 'like', '%'. $keyword. '%')
-            ->orderBy('created_at', 'DESC')
-            ->paginate(5);
-        }
+        $service->getPostSearchedByKeywords($inputKeyword);
         $categoryList[] = $category->getCategoryList();
         
         return view('search.keyword',compact('posts', 'inputKeyword', 'categoryList'));
-    }
-    
-    private function procKeywords(?string $input) :array
-    {
-        $keywords = str_replace('　', ' ', $input);
-        $keywords = preg_replace('/\s++/', ' ', $keywords);
-        return preg_split('/ ++/', $keywords);
     }
 }
---
+++ app/Services/SearchService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+
+class SearchService
+{   
+    /**
+     * 検索ワードの空白文字の変換と切り分け、部分一致検索
+     * 
+     * @param string $inputKeyword
+     * @return Object
+     */
+    public function getPostSearchedByKeywords(?string $inputKeyword) :object
+    {
+        $keywords = [];
+        $keywords = self::procKeywords($inputKeyword);
+        
+        foreach ($keywords as $keyword) {
+            $keyword = addcslashes($keyword, '%_\\');
+            $posts = Post::where('title', 'like', '%'. $keyword. '%')
+            ->orwhere('content', 'like', '%'. $keyword. '%')
+            ->orderBy('created_at', 'DESC')
+            ->paginate(5);
+        }
+        
+        return $posts;
+    }
+    
+    /**
+     * 全角スペースを半角スペースに変換後、連続する空白文字を1つの半角スペースに
+     * @param string $input
+     * @return array
+     */
+    private function procKeywords(?string $input) :array
+    {
+        $keywords = str_replace('　', ' ', $input);
+        $keywords = preg_replace('/\s++/', ' ', $keywords);
+        
+        return preg_split('/ ++/', $keywords);
+    }
+}
